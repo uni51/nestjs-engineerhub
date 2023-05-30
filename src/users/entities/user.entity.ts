6ebaaf440ad03bc240977a7983c03c4129220601
uni51 <unieye51@gmail.com>
2023-05-31 05:36:07 +0900

    データベースと接続してテーブルを作成

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// データベースのテーブルを定義する
+@Entity('users')
+export class User {
+  // 主キーを定義する
+  @PrimaryGeneratedColumn({
+    comment: 'アカウントID',
+  })
+  readonly id: number;
+
+  // カラムを定義する
+  @Column('varchar', { comment: 'アカウント名' })
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
